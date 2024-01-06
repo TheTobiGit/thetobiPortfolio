2f89477af378eb9f81ee9524ba38654b6a45337d
Gideon Sarfo <aemonsarfo@outlook.com>
2024-01-06 10:56:45 -0500

    Enhance site font and aesthetics with Google Fonts and Nuxt modules
    Introduce Tailwind CSS, Google Fonts, and icon support by incorporating respective Nuxt modules, updating the dependencies, and adding custom font styling. Removed the initial placeholder component on the index page and replaced it with a simple portfolio message. Updated the favicon to a new design, reflecting a rebranding effort. These changes aim to improve the user interface and prepare for further UI development.
    
    - Added `@nuxtjs/google-fonts` for font customizations
    - Integrated `@nuxtjs/tailwindcss` for utility-first styling
    - Included `nuxt-icon` for scalable icon support
    - Simplified the `index.vue` to a basic portfolio message
    - Updated favicon for a fresh visual identity

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "@nuxtjs/google-fonts"
+  ],
+
+  css: ['~/assets/css/main.css'],
+
+  app: {
+    head: {
+      title: "TheTobi"
+    }
+  }
 })
